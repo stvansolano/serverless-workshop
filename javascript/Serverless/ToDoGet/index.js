6da38b73137afc45c1e7d8359464d7a176ded97f
Esteban Solano G <stvansolano@outlook.com>
2019-10-08 22:37:42 -0600

    Updated GetToDos function

--- javascript/Serverless/ToDoGet/index.js
+++ javascript/Serverless/ToDoGet/index.js
@@ -1,16 +1,45 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.id || (req.body && req.body.id)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+let MongoClient = require('mongodb').MongoClient;
+
+const connectionString = process.env['MongoDbConnection'];
+
+module.exports = async function (context, req) {
+    context.log('Running');
+    console.log(`Using connection string:${connectionString}`);
+
+    let client = await MongoClient.connect(connectionString,
+        { useNewUrlParser: true });
+
+    let db = client.db('ToDos');
+    let collectionName = process.env['MongoDbCollection'];
+    let response = {};
+
+    console.log(`Using collectionName string:${collectionName}`);
+
+    try {
+        const result = await db.collection(collectionName)
+                               .find({}, { name: 1, _id: 1 })
+                               .toArray();
+
+        console.log(`result => ${JSON.stringify(result)}`);
+        console.log(`res => ${JSON.stringify(req.body)}`);
+
+        response = {
+            status: 200,
+            body: result,
+            headers: {
+                'Content-Type': 'application/json'
+            }
         };
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
+    catch(error){
+        console.log(error);
+    }
+    finally {
+        context.res = response;
+        client.close();
+        //context.done();
+
+        return response;
     }
 };
