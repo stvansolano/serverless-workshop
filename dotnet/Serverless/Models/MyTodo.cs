a1e0d0a226026ca79fa737a0e6a2e92e313722c0
Esteban Solano G <stvansolano@outlook.com>
2019-11-05 11:44:48 -0600

    Updated model (dotnet)

--- dotnet/Serverless/Models/MyTodo.cs
+++ dotnet/Serverless/Models/MyTodo.cs
@@ -1,11 +1,19 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
 
 namespace Serverless
 {
     public class MyTodo
     {
+
+        [BsonId]
+        [JsonProperty("_Id")]
+        public MongoDB.Bson.ObjectId _Id { get; set; }
+    
+        [JsonProperty]
         public string Id { get; set; }
+        
 
         [BsonElement("name")]
         public string Name { get; set; }
